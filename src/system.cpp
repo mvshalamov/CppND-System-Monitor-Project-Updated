5f3b60147a8a9e8a33bd6753d374aed75fdabdf2
mvshalamov <mvshalamov@gmail.com>
2021-06-16 17:11:12 +0300

    add cnrono

--- src/system.cpp
+++ src/system.cpp
@@ -20,9 +20,36 @@ Processor& System::Cpu() { return cpu_; }
 
 // DONE: Return a container composed of the system's processes
 vector<Process>& System::Processes() { 
+
+    /*
+    string Format::ElapsedTime(long s) {
+  std::chrono::seconds seconds{s};
+
+  // return std::chrono::format("%T", seconds); // in C++20 :-)
+
+  std::chrono::hours hours =
+      std::chrono::duration_cast<std::chrono::hours>(seconds);
+
+  seconds -= std::chrono::duration_cast<std::chrono::seconds>(hours);
+
+  std::chrono::minutes minutes =
+      std::chrono::duration_cast<std::chrono::minutes>(seconds);
+
+  seconds -= std::chrono::duration_cast<std::chrono::seconds>(minutes);
+
+  std::stringstream ss{};
+
+  ss << std::setw(2) << std::setfill('0') << hours.count()     // HH
+     << std::setw(1) << ":"                                    // :
+     << std::setw(2) << std::setfill('0') << minutes.count()   // MM
+     << std::setw(1) << ":"                                    // :
+     << std::setw(2) << std::setfill('0') << seconds.count();  // SS
+
+  return ss.str();
+    */
     vector<int> pids = LinuxParser::Pids();
     
-    processes_ = {};
+    processes_.clear();
     
     for (int pid : pids){
         processes_.emplace_back(Process(pid));
